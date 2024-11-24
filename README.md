bb4742cbd5de457af5f1730fa335e6d43393cb56
Delaney Gillilan <delaneygillilan@gmail.com>
2024-11-23 19:55:59 -0800

    add more info to readme, match original code

--- README.md
+++ README.md
@@ -13,10 +13,22 @@ task -w
 
 # What's different?
 
-1. About 350 lines of Go code and 150 of [Templ](https://templ.guide/) for UI.  Templ could be shorter but expanded lines for readability.
-2. I'm pretty sure it its the faster (queries take < 5ms on my machine) and the smaller memory footprint (24MB) of any of the apps in the original.
-3. It's only a one time cost of 12KiB of JS
-4. It handles speculation rules asynchronously for precaching the pokemon images.
-5. Use pure Go SQLite so N+1 queries are not a problem.
-6. It's a single binary with no dependencies.
-7. Does lazy loading of results and works well on 3G testing
+1. 321LOC of Go across 6 files (including HTTPServer) and ~125LOC of [Templ](https://templ.guide/) across 3 files for UI.  Could be one of each but tried to match the spirit of the original.
+   1. Templ could be shorter but expanded lines for readability.
+   2. Pretty sure it's the smallest codebase of any of the apps in the original.
+2. Metrics
+   1. I'm pretty sure it's the fastest (queries take < 1ms on my machine)
+   2. Smallest memory footprint (19MB sustained) of any of the apps in the original.
+3. It handles speculation rules asynchronously for precaching the pokemon images.
+4. Use pure Go SQLite so N+1 queries are not a problem.
+5. It's a single binary with no dependencies.
+6. Datastar
+   1. One time cost of 12KiB of JS.
+   2. Handles all UI interactions and backend communication.
+   3. The version is [ALL the plugins](https://data-star.dev/bundler) we are only using a handful of them for this demo
+   4. No websockets, just normal HTTP requests that can work on HTTP 2/3.
+
+I hope this clear up why it's not
+> [from a glance like Live View](https://x.com/theo/status/1858032204355612770)
+>
+> ~ [Theo](https://x.com/theo)
--- web/routes_results.go
+++ web/routes_results.go
@@ -1,76 +1,28 @@
 package web
 
 import (
-	"context"
 	"fmt"
 	"net/http"
-	"strconv"
-	"sync"
-	"time"
 
 	"github.com/delaneyj/toolbelt"
 	"github.com/go-chi/chi/v5"
 	"github.com/starfederation/1a4s-datastar/sql/zz"
-	datastar "github.com/starfederation/datastar/code/go/sdk"
 	"zombiezen.com/go/sqlite"
 )
 
-const (
-	blockSize    = 64
-	pokemonCount = 1024
-)
-
 func setupResultsRoutes(r chi.Router, db *toolbelt.Database) error {
-	rows := []zz.ResultsRes{}
-	mu := &sync.RWMutex{}
-
-	updateResults := toolbelt.Throttle(5*time.Second, func(ctx context.Context) error {
-		mu.Lock()
-		defer mu.Unlock()
-
-		rows = rows[:0]
-		if err := db.ReadTX(ctx, func(tx *sqlite.Conn) (err error) {
-			rows, err = zz.OnceResults(tx)
-			return err
-		}); err != nil {
-			return fmt.Errorf("error reading results: %w", err)
-		}
-
-		return nil
-	})
-
 	r.Route("/results", func(resultsRouter chi.Router) {
 		resultsRouter.Get("/", func(w http.ResponseWriter, r *http.Request) {
 			ctx := r.Context()
-			updateResults(ctx)
-			ResultsPage().Render(ctx, w)
-		})
-
-		resultsRouter.Get("/{blockOffsetRaw}", func(w http.ResponseWriter, r *http.Request) {
-			blockOffsetRaw := chi.URLParam(r, "blockOffsetRaw")
-			blockOffset, err := strconv.Atoi(blockOffsetRaw)
-
-			sse := datastar.NewSSE(w, r)
-			if err != nil {
-				sse.ConsoleError(err)
+			var rows []zz.ResultsRes
+			if err := db.ReadTX(ctx, func(tx *sqlite.Conn) (err error) {
+				rows, err = zz.OnceResults(tx)
+				return err
+			}); err != nil {
+				http.Error(w, fmt.Sprintf("error reading results: %v", err), http.StatusInternalServerError)
 				return
 			}
-
-			if blockOffset*blockSize >= pokemonCount {
-				sse.RemoveFragments("#loadNextBlock")
-				return
-			}
-
-			mu.RLock()
-			defer mu.RUnlock()
-			blockRows := rows[blockOffset*blockSize : (blockOffset+1)*blockSize]
-			sse.MergeFragmentTempl(
-				resultsRows(blockOffset, blockRows...),
-				datastar.WithSelectorID("results"),
-				datastar.WithMergeAppend(),
-			)
-
-			sse.MergeFragmentTempl(loadNextBlock(blockOffset))
+			ResultsPage(rows...).Render(ctx, w)
 		})
 	})
 	return nil
